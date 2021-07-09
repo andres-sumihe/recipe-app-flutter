0d6412d00719d6cd0fdf7924d076c6ad3e7d30d2
andres-sumihe <672018136@student.uksw.edu>
2021-07-10 00:38:52 +0900

    remove unused widget

--- lib/screens/signin/signin_screen.dart
+++ lib/screens/signin/signin_screen.dart
@@ -191,30 +191,6 @@ class _LoginScreenState extends State<SignInScreen> {
     );
   }
 
-  Widget _buildSocialBtn(Function onTap, AssetImage logo) {
-    return GestureDetector(
-      onTap: onTap(),
-      child: Container(
-        height: 60.0,
-        width: 60.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              offset: Offset(0, 2),
-              blurRadius: 6.0,
-            ),
-          ],
-          image: DecorationImage(
-            image: logo,
-          ),
-        ),
-      ),
-    );
-  }
-
   Widget _buildSignupBtn() {
     return GestureDetector(
       onTap: () => print('Sign Up Button Pressed'),
